34cf2faf9d5e3d54adc7430070dd8eb612dbe5e6
Guido Beggiato <beggio.guido@gmail.com>
2023-05-18 07:32:14 +0000

    added pandas example

--- iris.py
+++ iris.py
@@ -1,22 +1,12 @@
-from concurrent.futures import ThreadPoolExecutor as ThreadPool
-from itertools import repeat, product, combinations_with_replacement
-from os import system
-from pathlib import Path
-from pprint import pprint
-from random import choices, randint
-from time import perf_counter
-from typing import Any, Callable, Iterable
-
-from pandas import DataFrame as PandasDF, read_csv
+from pandas import read_csv
 
 
 def main():
-    _ = system("cls")
 
     iris_url = "https://j.mp/iriscsv"
-    iris_url = (Path(__file__).parent.parent / "iris.txt").read_text()
 
     data = read_csv(iris_url)
+    
     data.columns = data.columns.str.upper()
 
     # some analysis params
@@ -33,25 +23,24 @@ def main():
         .groupby("SPECIES")
         .agg({
             "SEPAL_LENGTH": "max",
-            "PETAL_LENGTH":"min"
+            "PETAL_LENGTH":"min",
+            "OK_WIDTH": "count"
         })
         # resetting index (if you want to keep it)
         .reset_index(drop=False)
         # renaming
         .rename(columns={
             "SEPAL_LENGTH": "MAX_SEPAL_LENGTH",
-            "PETAL_LENGTH":"MIN_PETAL_LENGTH" 
+            "PETAL_LENGTH":"MIN_PETAL_LENGTH",
+            "OK_WIDTH":"COUNT_OK_WIDTH"  
         })
         # filtering
         .query("MAX_SEPAL_LENGTH > @min_length_accepted")
     )
 
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.merge.html
+
     print(analysis)
-  
-
-
-
-    
 
     return 0
 
